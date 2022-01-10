e10da11fb585c27ec51bf5a7623eaae2d2094db3
MathAdmin <beon@gmx.ch>
2022-01-10 19:34:38 +0100

    Test WWW4

--- src/problem/generator/sinTriangle.ts
+++ src/problem/generator/sinTriangle.ts
@@ -34,11 +34,7 @@ const renderDescription= (params: Params, keys: string[]): string => {
 
 const renderSolution = (params: Params, keys: string[]): string => {
   if (keys[0].length+keys[1].length+keys[2].length<5){
-    return `
-    \\begin{align*}
-    \\\doubleR
-    \\end{align*}
-    `;
+    return "\doubleR";
   } else {
     const values = sliceIntoChunks(keys, 3)
       .map((chunk) => chunk.map((key) => `${key.replaceAll("alpha", "\\alpha").replaceAll("beta", "\\beta").replaceAll("gamma", "\\gamma")}&=${params[key]}`).join(" & "))
