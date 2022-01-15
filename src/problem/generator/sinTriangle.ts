77f74675424f90eeea1afab92c37b69a2b474b3a
MathAdmin <beon@gmx.ch>
2022-01-15 12:55:16 +0100

    Test round

--- src/problem/generator/sinTriangle.ts
+++ src/problem/generator/sinTriangle.ts
@@ -34,9 +34,9 @@ export const startParams = (variant:Variant): Params => {
       s2 = Math.floor(Math.random() * 1000)/100;
       s3 = Math.floor(Math.random() * 1000)/100;
       if ((s1+s2>s3) && (s1+s3>s2) && (s2+s3>s1)) {
-        w1 = Math.acos((s2*s2+s3*s3-s1*s1)/(2*s2*s3))
-        w2 = Math.acos((s1*s1+s3*s3-s2*s2)/(2*s1*s3))
-        w3 = Math.acos((s2*s2+s1*s1-s3*s3)/(2*s2*s1))
+        w1 = Math.round((Math.acos((s2*s2+s3*s3-s1*s1)/(2*s2*s3)))*1000)/1000;
+        w2 = Math.round(Math.acos((s1*s1+s3*s3-s2*s2)/(2*s1*s3))*1000)/1000;
+        w3 = Math.round(Math.acos((s2*s2+s1*s1-s3*s3)/(2*s2*s1))*1000)/1000;
       }
       break;
     case Variant.SSW:
