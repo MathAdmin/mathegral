18272d4524e6f8f7ad28d540b16e0a49c57e7ddc
MathAdmin <beon@gmx.ch>
2022-01-18 22:33:04 +0100

    Test0000

---
+++ src/problem/generator/fractionalEquation.ts
@@ -0,0 +1,32 @@
+import { ProblemGenerator } from "../ProblemGeneratorSpi";
+import { randomInt } from "../util/randomizer";
+
+export const renderEquation = (s1: number, s2: number) => {
+  return `x^2+${-s1 - s2}x+${s1 * s2}=0`
+    .replaceAll("+-", "-")
+    .replace(/([+-])1x/, "$1x");
+};
+
+export const renderFactorization = (s1: number, s2: number) => {
+  return `(x-${s1})(x-${s2})=0`.replaceAll("+-", "-").replaceAll("--", "+");
+};
+
+const fractionalEquation: ProblemGenerator = {
+  key: "quadratic-equation",
+  generate: () => {
+    const s1 = randomInt(-9, 10, (value) => value !== 0);
+    const s2 = randomInt(-9, 10, (value) => ![0, s1, -s1].includes(value));
+
+    return {
+      description: renderEquation(s1, s2),
+      solution: `
+        \\begin{aligned}
+          ${renderFactorization(s1, s2).replace("=", "&=")} \\\\
+          x&=\\begin{cases} ${s1} \\\\ ${s2} \\end{cases}
+        \\end{aligned}
+      `,
+    };
+  },
+};
+
+export default fractionalEquation;
