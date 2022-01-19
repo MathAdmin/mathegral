67cf94f7d50b5f4c1feeee2b420ad4136763d664
MathAdmin <beon@gmx.ch>
2022-01-19 10:01:21 +0100

    Test 2fraction

--- src/problem/generator/fractionalEquation.ts
+++ src/problem/generator/fractionalEquation.ts
@@ -19,9 +19,11 @@ export const renderEquation = (params: Params) => {
   const b1 = params[1][1];
   const c1 = params[1][2];
   const d1 = params[1][3];
+  const b2 = params[2][1];
+  const c2 = params[2][2];
+  const d2 = params[2][3];
 
-
-  return `\\frac{${b1}}{${c1}x+${d1}}=0`
+  return `\\frac{${b1}}{${c1}x+${d1}}+\\frac{${b2}}{${c2}x+${d2}}=0`
   .replaceAll("+-", "-")
   .replaceAll("{1x", "{x");
 };
@@ -38,18 +40,19 @@ const fractionalEquation: ProblemGenerator = {
     var b1 = randomInt(-9, 10, (value) => value !== 0);
     var c1 = randomInt(-9, 10, (value) => value !== 0);
     var d1 = randomInt(-9, 10, (value) => value !== 0);
-    //var c2 = randomInt(-9, 10, (value) => value !== 0);
-    //var d2 = randomInt(-9, 10, (value) => ![0, d1].includes(value));
+    var b2 = randomInt(-9, 10, (value) => value !== 0);
+    var c2 = randomInt(-9, 10, (value) => value !== 0);
+    var d2 = randomInt(-9, 10, (value) => ![0, d1].includes(value));
 
 
     var sol = [sol1,0,0,0];
-    var frac1 = [0,0,0,0];
-    var frac2 = [0,0,0,0];
+    var frac1 = [0,b1,c1,d1];
+    var frac2 = [0,b2,c2,d2];
     var frac3 = [0,0,0,0];
     var frac4 = [0,0,0,0];
     var pol = [0,0,0,0];
 
-    let params:Params = [[sol1,0,0,0],[0,b1,c1,d1],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    let params:Params = [[sol1,0,0,0],[0,b1,c1,d1],[0,b2,c2,d2],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 
 
 
