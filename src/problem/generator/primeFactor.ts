93f822709b1d4498941c0c0aa0f6324d425e73f6
MathAdmin <beon@gmx.ch>
2022-02-10 09:07:09 +0100

    Prime Factorization 2

--- src/problem/generator/primeFactor.ts
+++ src/problem/generator/primeFactor.ts
@@ -6,34 +6,26 @@ const primeFactor: ProblemGenerator = {
   key: "prime-factor",
   generate: () => {
 
-    let primeweight = [
-      [2,1,0],[3,2,0],[5,3,0],[7,4,0],[11,4,0],[13,4,0],
-      [17,5,0],[19,6,0],[23,7,0],[29,8,0]
+    let primelist = [
+      [2,0],[3,0],[5,0],[7,0],[11,0],[13,0],
+      [17,0],[19,0],[23,0],[29,0],[31,0],[37,0]
     ];
-    const maxweight = 12;
-    const maxnumber = 500;
-    var weightsum = 0;
+  
+    const maxnumber = 600;
     var factornumber = 1;
-    const maxliste = primeweight.length;
+    const maxliste = primelist.length;
     var pos = 0;
-    var morefactor = true;
-        while (morefactor){
-          pos = randomInt(0,maxliste);
-          if (weightsum + primeweight[pos][1]< maxweight) {
-            weightsum = weightsum + primeweight[pos][1];
-            primeweight[pos][2] = primeweight[pos][2] + 1;
-            factornumber = factornumber * primeweight[pos][0];
-          }
-          if ((factornumber > maxnumber) || (weightsum > 10)){
-            morefactor = false;
-          }
-      
+    while (factornumber < maxnumber){
+        pos = randomInt(0,maxliste);
+        primelist[pos][1] = primelist[pos][1] + 1;
+        factornumber = factornumber * primelist[pos][0];
     }
-    var pos = 0;
+  
+    pos = 0;
     var factorstring = ``;
     while (pos < maxliste){
-      if (primeweight[pos][2]>0){
-        factorstring = factorstring + ` \\cdot ${primeweight[pos][0]}^${primeweight[pos][2]}` 
+      if (primelist[pos][1]>0){
+        factorstring = factorstring + ` \\cdot ${primelist[pos][0]}^${primelist[pos][1]}` 
       }
       pos = pos + 1;
     }
