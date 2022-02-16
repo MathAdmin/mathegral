ca3e9e7d2ff840f6292ef31b119b69603bdd0579
MathAdmin <beon@gmx.ch>
2022-02-16 10:32:42 +0100

    Distribution1

--- src/problem/generator/primeFactor.ts
+++ src/problem/generator/primeFactor.ts
@@ -1,6 +1,12 @@
 import { ProblemGenerator } from "../ProblemGeneratorSpi";
-import { randomInt } from "../util/randomizer";
+import { randomdist } from "../util/randomDistribution";
 
+
+
+let primedistribution = [
+  [2,30],[3,60],[5,70],[7,77],[11,83],[13,88],
+  [17,90],[19,92],[23,94],[29,96],[31,98],[37,100]
+];
 
 const primeFactor: ProblemGenerator = {
   key: "prime-factor",
@@ -11,14 +17,18 @@ const primeFactor: ProblemGenerator = {
       [17,0],[19,0],[23,0],[29,0],[31,0],[37,0]
     ];
   
-    const maxnumber = 600;
+    const minnumber = 200;
+    const maxnumber = 1500;
     var factornumber = 1;
     const maxliste = primelist.length;
     var pos = 0;
-    while (factornumber < maxnumber){
-        pos = randomInt(0,maxliste);
-        primelist[pos][1] = primelist[pos][1] + 1;
-        factornumber = factornumber * primelist[pos][0];
+    while (factornumber < minnumber){
+        pos = randomdist(primedistribution);
+        if (factornumber * primelist[pos][0]<maxnumber){
+          primelist[pos][1] = primelist[pos][1] + 1;
+          factornumber = factornumber * primelist[pos][0];
+        }
+        
     }
   
     pos = 0;
